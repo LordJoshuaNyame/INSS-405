b95c99bd32e8d884bcb1d3d57eb29de800e78f7c
LordJoshuaNyame <124842379+LordJoshuaNyame@users.noreply.github.com>
2023-05-25 03:20:00 -0400

    Add files via upload
    Codes for Exam 2

---
+++ q2.py
@@ -0,0 +1,19 @@
+import random
+
+# Generate 100 random numbers
+random_numbers = []
+for i in range(100):
+    num = random.randint(1, 10000)
+    random_numbers.append(num)
+
+def sort_numbers(numbers):
+    num=numbers.sort()
+    return num
+
+print('Random Numbers (before sorting):')
+
+print(random_numbers)
+#Sort the Numbers
+sortedArray=sort_numbers(random_numbers)
+print('\nSorted Numbers:')
+print(random_numbers)
